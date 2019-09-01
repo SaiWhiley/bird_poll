3b23d9c6f62aa4dfe385ef91b97ef373ceaab6b7
Sai Whiley <sai.whiley196@gmail.com>
2019-09-01 13:15:15 +1000

    localTweet model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190831041538) do
+ActiveRecord::Schema.define(version: 20190901031429) do
+
+  create_table "stored_tweets", force: :cascade do |t|
+    t.string "tweet_id"
+    t.text "content"
+    t.string "posted_at"
+    t.string "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "TwitterID"
